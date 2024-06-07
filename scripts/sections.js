08799ff12fd117c768d5f5dbea54626e22fd3096
Burmuruk <santoyo.herrera.david@gmail.com>
2024-06-06 20:43:26 -0600

    Main finished
    there's not info about me and project's page just has a footer.

--- scripts/sections.js
+++ scripts/sections.js
@@ -3,32 +3,72 @@ document.addEventListener("DOMContentLoaded", SaveSections);
 let videoSection = 0;
 let projectsSection = 0;
 let curriculumSection = 0;
+let currentElement = null;
 
 function SaveSections(){
     videoSection = document.getElementById("DemoReelSection");
     projectsSection = document.getElementById("projectsSection");
+    curriculumSection = document.getElementById("cvSection");
     
     document.getElementById("sectionsContainer").removeChild(projectsSection);
+    document.getElementById("sectionsContainer").removeChild(curriculumSection);
+    document.getElementById("sectionsContainer").removeChild(videoSection);
+    // currentElement = videoSection;
 }
 
 function showDemoReel(){
-    if (videoSection == 0) return;
 
-    let child = document.getElementById("sectionsContainer").lastChild;
+    if (currentElement == videoSection){
+        document.getElementById("sectionsContainer").removeChild(currentElement);
+        currentElement = null;
+        return;
+    }
+    
+    if (currentElement != null)
+        document.getElementById("sectionsContainer").removeChild(currentElement);
 
-    if (child == null) return;
-
-    document.getElementById("sectionsContainer").removeChild(child);
     document.getElementById("sectionsContainer").append(videoSection);
+    currentElement = videoSection;
 }
 
 function showDemoProjects(){
-    if (projectsSection == 0) return;
 
-    let child = document.getElementById("sectionsContainer").lastChild;
+    if (currentElement == projectsSection){
+        document.getElementById("sectionsContainer").removeChild(currentElement);
+        currentElement = null;
+        return;
+    }
+    
+    if (currentElement != null)
+        document.getElementById("sectionsContainer").removeChild(currentElement);
 
-    if (child == null) return;
-
-    document.getElementById("sectionsContainer").removeChild(child);
     document.getElementById("sectionsContainer").append(projectsSection);
+    currentElement = projectsSection;
+}
+
+function showCV(){
+    
+    if (currentElement == curriculumSection){
+        document.getElementById("sectionsContainer").removeChild(currentElement);
+        currentElement = null;
+        return;
+    }
+    
+    if (currentElement != null)
+        document.getElementById("sectionsContainer").removeChild(currentElement);
+
+    document.getElementById("sectionsContainer").append(curriculumSection);
+    currentElement = curriculumSection;
+}
+
+function openProject(id){
+    console.log(id);
+    switch (id) {
+        case 0:
+            window.location = "pages/navMesh.html"
+            break;
+    
+        default:
+            break;
+    }
 }
